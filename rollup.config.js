6ac7130a7d7846a180bb375425b1e34f0c0e2ee9
joshbartley <5738532+joshbartley@users.noreply.github.com>
2021-12-05 13:31:35 -0500

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import babel from 'rollup-plugin-babel'
-import { terser } from 'rollup-plugin-terser'
 import commonjs from 'rollup-plugin-commonjs'
 import vue from 'rollup-plugin-vue'
 import postcss from 'rollup-plugin-postcss'
